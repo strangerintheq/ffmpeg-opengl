90fe370ea8cb17cea2f45814fe2b77a044f6bdfa
strangerintheq <tripmannn@gmail.com>
2019-06-06 22:39:04 +0300

    shader load from file

--- vf_genericshader.c
+++ vf_genericshader.c
@@ -1,5 +1,6 @@
 #include "libavutil/opt.h"
 #include "internal.h"
+#include <stdio.h>
 
 #ifdef __APPLE__
 #include <OpenGL/gl3.h>
@@ -13,18 +14,21 @@ static const float position[12] = {
   -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f};
 
 static const GLchar *v_shader_source =
-  "attribute vec2 position;\n"
-  "varying vec2 texCoord;\n"
+  "#version 150\n"
+  "in vec2 position;\n"
+  "out vec2 texCoord;\n"
   "void main(void) {\n"
-  "  gl_Position = vec4(position, 0, 1);\n"
+  "  gl_Position = vec4(position.x, position.y, 0.0, 1.0);\n"
   "  texCoord = position;\n"
   "}\n";
 
 static const GLchar *f_shader_source =
+  "#version 150\n"
   "uniform sampler2D tex;\n"
-  "varying vec2 texCoord;\n"
+  "in vec2 texCoord;\n"
+  "out vec4 fragColor;\n"
   "void main() {\n"
-  "  gl_FragColor = texture2D(tex, texCoord * 0.5 + 0.5);\n"
+  "  fragColor = texture(tex, texCoord * 0.5 + 0.5);\n"
   "}\n";
 
 #define PIXEL_FORMAT GL_RGB
@@ -34,6 +38,7 @@ typedef struct {
   GLuint        program;
   GLuint        frame_tex;
   GLFWwindow    *window;
+  GLuint        vao;
   GLuint        pos_buf;
 } GenericShaderContext;
 
@@ -51,10 +56,20 @@ static GLuint build_shader(AVFilterContext *ctx, const GLchar *shader_source, GL
   glCompileShader(shader);
   GLint status;
   glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+  if (status == GL_FALSE) {
+    GLint log_length = 0;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+    GLchar *log_buffer = av_malloc(log_length);
+    glGetShaderInfoLog(shader, log_length, &log_length, log_buffer);
+    av_log(ctx, AV_LOG_ERROR, "GLSL %s\n", log_buffer);
+    av_free(log_buffer);
+  }
   return status == GL_TRUE ? shader : 0;
 }
 
 static void vbo_setup(GenericShaderContext *gs) {
+  glGenVertexArrays(1, &gs->vao);
+  glBindVertexArray(gs->vao);
   glGenBuffers(1, &gs->pos_buf);
   glBindBuffer(GL_ARRAY_BUFFER, gs->pos_buf);
   glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
@@ -85,9 +100,27 @@ static void tex_setup(AVFilterLink *inlink) {
 static int build_program(AVFilterContext *ctx) {
   GLuint v_shader, f_shader;
   GenericShaderContext *gs = ctx->priv;
+  GLchar *frag_shader_source = NULL;
+  FILE *glsl_file = fopen("genericshader.glsl", "r");
+  if (glsl_file) {
+    fseek(glsl_file, 0, SEEK_END);
+    long size = ftell(glsl_file);
+    fseek(glsl_file, 0, SEEK_SET);
+    if (size >= 0) {
+      frag_shader_source = calloc(1, size + 1);
+      size = fread(frag_shader_source, 1, size, glsl_file);
+      frag_shader_source[size] = '\0';
+    }
+  } else {
+    frag_shader_source = (GLchar*) f_shader_source;
+  }
 
   if (!((v_shader = build_shader(ctx, v_shader_source, GL_VERTEX_SHADER)) &&
-        (f_shader = build_shader(ctx, f_shader_source, GL_FRAGMENT_SHADER)))) {
+        (f_shader = build_shader(ctx, frag_shader_source, GL_FRAGMENT_SHADER)))) {
+    if (glsl_file) {
+      free(frag_shader_source);
+      fclose(glsl_file);
+    }
     return -1;
   }
 
@@ -95,6 +128,11 @@ static int build_program(AVFilterContext *ctx) {
   glAttachShader(gs->program, v_shader);
   glAttachShader(gs->program, f_shader);
   glLinkProgram(gs->program);
+
+  if (glsl_file) {
+    free(frag_shader_source);
+    fclose(glsl_file);
+  }
 
   GLint status;
   glGetProgramiv(gs->program, GL_LINK_STATUS, &status);
@@ -109,6 +147,10 @@ static int config_props(AVFilterLink *inlink) {
   AVFilterContext     *ctx = inlink->dst;
   GenericShaderContext *gs = ctx->priv;
 
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
   glfwWindowHint(GLFW_VISIBLE, 0);
   gs->window = glfwCreateWindow(inlink->w, inlink->h, "", NULL, NULL);
 
@@ -156,6 +198,7 @@ static av_cold void uninit(AVFilterContext *ctx) {
   glDeleteTextures(1, &gs->frame_tex);
   glDeleteProgram(gs->program);
   glDeleteBuffers(1, &gs->pos_buf);
+  glDeleteVertexArrays(1, &gs->vao);
   glfwDestroyWindow(gs->window);
 }
 
